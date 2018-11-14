5bb399efb2028347eefa0efe28e752c2daf10bed
Adalon <etienne.duverney@viacesi.fr>
2018-11-14 22:05:27 +0100

    Checkpoint

--- clock/main.js
+++ clock/main.js
@@ -14,11 +14,19 @@ function setClock() {
     h = (h < 10) ? `0${h}` : h;
     m = (m < 10) ? `0${m}` : m;
     s = (s < 10) ? `0${s}` : s;
-
     const string = `${h}:${m}:${s} ${after}`;
     document.getElementById('clock2').innerText = string;
+
+    var d = new Date()
+    rotate(sec, 6 * s)
+    rotate(min, 6 * m)
+    rotate(hour, 30 * (h % 12) + m / 2)
 }
 
 function toggleActive(element) {
     element.classList.toggle('active');
+}
+
+function rotate(element, deg) {
+    element.setAttribute('transform', 'rotate(' + deg + ' 50 50)')
 }
